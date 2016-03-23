f3fb78b3c241aa34a28ec82c7e68d85541a85863
Gustavo Ardito <gustavo.ardito@globallogic.com>
2016-03-23 11:36:56 -0300

    Add Files to implement autofac

---
+++ AutofacExample/ConsoleOutputService .cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AutofacExample
+{
+    public class ConsoleOutputService : IOutputService
+    {
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Magic 8 Ball simulator");
+        }
+
+        public void PrintInputPrompt()
+        {
+            Console.Write("Ask a Question, or press [Enter] to exit >> ");
+        }
+
+        public void PrintExit()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
---
+++ AutofacExample/ContainerSetup.cs
@@ -0,0 +1,30 @@
+﻿namespace AutofacExample
+{
+    using System.IO;
+
+    using Autofac;
+
+    public class ContainerSetup
+    {
+        private ContainerBuilder builder;
+
+        public IContainer BuildContainer()
+        {
+            builder = new ContainerBuilder();
+
+            // in English this reads, setup ConsoleInputService as the implementation of 
+            // IInputService
+            builder.RegisterType<ConsoleInputService>().As<IInputService>();
+
+            // Magic8BallSimulator doesn't implement an interface, its registered as-is
+            builder.RegisterType<Magic8BallSimulator>();
+            builder.RegisterType<MessageService>().As<IMessageService>();
+
+            // builder.RegisterType<ConsoleOutputService>().As<IOutputService>();
+            string outputFilePath = Path.Combine(Path.GetTempPath(), "magic8BallOutput.txt");
+            builder.Register(c => new MultipleOutputService(outputFilePath)).As<IOutputService>();
+
+            return builder.Build();
+        }
+    }
+}
